3ce1bc8686d38ea315540cdc8db1af6bf84fb262
marishkazachariah <marishka.zachariah@gmail.com>
2023-09-29 16:05:49 +0200

    create Book class with viewBooks() method

---
+++ Book.java
@@ -0,0 +1,14 @@
+public class Book {
+	public static void main(String[] args) {
+		public void viewBooks(List<Book> books) {
+			if(books.isEmpty()) {
+				System.out.println("No books here.");
+			} else {
+				System.out.println("List of books: ");
+				for (int i = 0; i < books.length; i++) {						System.out.println(books[i].title + "by : " + books[i].author;
+				}
+			}
+
+		}
+	}
+}
