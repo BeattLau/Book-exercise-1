636dd36ae87f2ee66c4b7cf47df2e65c1fb0b32d
marishkazachariah <marishka.zachariah@gmail.com>
2023-09-29 17:30:47 +0200

    bug fixes to class

--- Book.java
+++ Book.java
@@ -1,18 +1,19 @@
+import java.util.List;
+
 public class Book {
-	public static void main(String[] args) {
 		public void viewBooks(List<Book> books) {
 			if(books.isEmpty()) {
 				System.out.println("No books here.");
 			} else {
 				System.out.println("List of books: ");
-				for (int i = 0; i < books.length; i++) {						System.out.println(books[i].title + "by : " + books[i].author;
+				for (Book book : books) {
+                			System.out.println(book);
 				}
 			}
 		}
 
-		public void removeBook(List<Book> books, string bookTitle) {
-			books.removeIf(book -> book.title.equals(bookTitle));
-			System.out.println("Book removed: " + title);
-		{
-	}
+//		public void removeBook(List<Book> books, String bookTitle) {
+//			books.removeIf(book -> book.title.equals(bookTitle));
+//			System.out.println("Book removed: " + title);
+//		}
 }
